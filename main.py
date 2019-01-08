f03fbf8a820e98f381cf4980a9c1b1ce4339250e
Barrypp.zzx <barry-hp@163.com>
2019-01-08 12:38:48 +0800

    add exif time change

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import re
 import time
+from datetime import datetime, timedelta
 from pathlib import Path
+
+import piexif
 
 info = re.compile('Blame([0-9]+)-([0-9]+)')
 p = Path('data')
@@ -12,6 +15,13 @@ for x in p.iterdir():
     hour = int(i.group(1))
     num = int(i.group(2))
     #
-    t = time.mktime((2019,1,8,hour,0,num,0,0,-1))
-    os.utime(x,(t,t))
+    t = datetime(2019,1,8,hour) + timedelta(0,num)
+    os.utime(x,(t.timestamp(),t.timestamp()))
+    #
+    if x.suffix == '.jpg':
+        exif_dict = piexif.load(str(x))
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = t.strftime('%Y:%m:%d %H:%M:%S')
+        exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = t.strftime('%Y:%m:%d %H:%M:%S')
+        piexif.insert(piexif.dump(exif_dict), str(x))
+    #
     print(x.name)
