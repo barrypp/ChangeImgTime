baa56095e7f00651e4ae507892b9594ed0fa5817
Barrypp.zzx <barry-hp@163.com>
2019-01-08 23:53:43 +0800

    fix bug: modify time should be set at last

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import piexif
 
 info = re.compile(r'\(v([0-9]+)\) - p([0-9]+)')
+#info = re.compile(r' - c([0-9]+).+ - p([0-9]+)')
 p = Path('data3')
 count = 0
 
@@ -15,9 +16,7 @@ for x in p.rglob('*.*'):
     i = info.search(x.name)
     hour = int(i.group(1))
     num = int(i.group(2))
-    #
     t = datetime(2019,1,1) + timedelta(seconds=num,hours=hour)
-    os.utime(x,(t.timestamp(),t.timestamp()))
     #
     if x.suffix == '.jpg':
         exif_dict = piexif.load(str(x))
@@ -25,5 +24,7 @@ for x in p.rglob('*.*'):
         exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = t.strftime('%Y:%m:%d %H:%M:%S')
         piexif.insert(piexif.dump(exif_dict), str(x))
     #
+    os.utime(x,(t.timestamp(),t.timestamp()))
+    #  
     count += 1
     print(count,x.name)
